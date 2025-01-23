71aade82ed023f9f94481ae60d01f3134babacf5
windbell0711 <lhn18117083808@163.com>
2025-01-23 15:39:11 +0800

    seperate intelligence.py from display.py

--- intelligence.py
+++ intelligence.py
@@ -10,33 +10,28 @@
 class Intelligence:
     def __init__(self, beach, war):
         self.beach = beach
-        self.war = war
+        self.war = war  # 尽量别用self.war哦
         self.mycamp = war.mycamp
 
-    Chn = []
-    Intl = []
-    king_p = 90
-    shuai_p = 90
+        self.Chn = []
+        self.Intl = []
+        self.king_p = 90
+        self.shuai_p = 90
 
     def get_attack_pose(self):
         self.reset_attack_pose()
-        print(self.beach[3])
-        for i in range(0, 89):
-            if not self.beach[i] is None:
-                print(self.beach[i].typ)
-                if self.beach[i].typ == 12:
-                    self.king_p = i
-                    print(i, self.beach[i].p, self.king_p, self.beach[i])
-                    break
-        for i in (63, 64, 65, 73, 74, 75, 83, 84, 85):
+        for i in self.beach.pieces:  # 找king
+            if i.typ == 12:
+                self.king_p = i.p
+                break
+        for i in (63, 64, 65, 73, 74, 75, 83, 84, 85):  # 找shuai
             if not self.beach[i] is None:
                 if self.beach[i].typ == 6:
                     self.shuai_p = i
-                    print(self.beach[i].p)
                     break
-        for i in self.beach:
-            if i is not None:
-                if i.camp_intl == True:  # 遍历国际象棋棋子
+        for i in self.beach.pieces:
+            if i.alive:
+                if i.camp_intl:  # 遍历国际象棋棋子
                     self.Intl += i.get_ma()
                 else:
                     self.Chn += i.get_ma()
@@ -47,48 +42,47 @@ class Intelligence:
         self.king_p = 90
         self.shuai_p = 90
 
-    def end(self):
-        self.beach.virtual_move(self.i, self.i.p)
-        self.beach.virtual_move(self.k, self.j)
+    def end(self, i, j, k):
+        self.beach.virtual_move(i, i.p)
+        self.beach.virtual_move(k, j)
 
-    def bgn(self):
-        self.beach.virtual_move(self.i, self.j)
-        self.beach.virtual_move(None, self.i.p)
-        print(self.beach[3])
+    def bgn(self, i, j):
+        self.beach.virtual_move(i, j)
+        self.beach.virtual_move(None, i.p)
         self.get_attack_pose()
 
     def king_is_checkmate(self):
-        for self.i in self.beach:
-            if not self.i is None:
-                if self.i.camp_intl == True:
-                    for self.j in self.i.get_ma():
-                        if self.j is not None:
-                            self.k = self.beach[self.j]
+        for i in self.beach:
+            if i is not None:
+                if i.camp_intl:
+                    for j in i.get_ma():
+                        if j is not None:
+                            k = self.beach[j]
                         else:
-                            self.k = None
-                        self.bgn()
-                        if self.mycamp == True:
+                            k = None
+                        self.bgn(i, j)
+                        if self.mycamp == False:
                             if not self.king_p in self.Chn:
-                                print(self.i.p, self.j)
-                                self.end()
+                                print(i.p, j)
+                                self.end(i, j, k)
                                 return False
-                        self.end()
+                        self.end(i, j, k)
         return True
 
     def shuai_is_checkmate(self):
-        for self.i in self.beach:
-            if not self.i is None:
-                if self.i.camp_intl == False:
-                    for self.j in self.i.get_ma():
-                        if self.j is not None:
-                            self.k = self.beach[self.j]
+        for i in self.beach:
+            if i is not None:
+                if not i.camp_intl:
+                    for j in i.get_ma():
+                        if j is not None:
+                            k = self.beach[j]
                         else:
-                            self.k = None
-                        self.bgn()
-                        if self.mycamp == False:
+                            k = None
+                        self.bgn(i, j)
+                        if self.mycamp == True:
                             if not self.shuai_p in self.Intl:
-                                print(self.i.p, self.j)
-                                self.end()
+                                print(i.p, j)
+                                self.end(i, j, k)
                                 return False
-                        self.end()
+                        self.end(i, j, k)
         return True
